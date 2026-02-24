c50cc2660a12aedfacd85ba0a26b9d1e152e7f9b
Alex Wicks <alex@awicks.io>
2026-02-24 14:06:38 +0000

    fix: pin zen flake to work around duplicate hash issue

--- modules/inputs/zen-browser.nix
+++ modules/inputs/zen-browser.nix
@@ -1,7 +1,9 @@
 { lib, ... }:
 {
   flake-file.inputs.zen-browser = {
-    url = lib.mkDefault "github:0xc000022070/zen-browser-flake";
+    # Pinned to commit with updated twilight hashes (Feb 23, 2026)
+    # See: https://github.com/0xc000022070/zen-browser-flake/commit/9ee8fb00d7333b1b2b65d686b160da19a57d5730
+    url = lib.mkDefault "github:0xc000022070/zen-browser-flake/9ee8fb00d7333b1b2b65d686b160da19a57d5730";
     inputs.nixpkgs.follows = lib.mkDefault "nixpkgs-unstable";
   };
 }
