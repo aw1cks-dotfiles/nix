72f946b1f73c6db84048bf170f5b8133ee3a576b
Alex Wicks <alex@awicks.io>
2026-02-25 10:12:49 +0000

    chore: bump nix flake lock

--- modules/inputs/zen-browser.nix
+++ modules/inputs/zen-browser.nix
@@ -1,9 +1,7 @@
 { lib, ... }:
 {
   flake-file.inputs.zen-browser = {
-    # Pinned to commit with updated twilight hashes (Feb 23, 2026)
-    # See: https://github.com/0xc000022070/zen-browser-flake/commit/9ee8fb00d7333b1b2b65d686b160da19a57d5730
-    url = lib.mkDefault "github:0xc000022070/zen-browser-flake/9ee8fb00d7333b1b2b65d686b160da19a57d5730";
+    url = lib.mkDefault "github:0xc000022070/zen-browser-flake";
     inputs.nixpkgs.follows = lib.mkDefault "nixpkgs-unstable";
   };
 }
