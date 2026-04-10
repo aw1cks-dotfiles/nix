5ec07da15310ca79acc213811db51ddf81a24509
Alex Wicks <alex@awicks.io>
2026-04-10 18:34:35 +0100

    refactor(home): manage neovim via home-manager module

---
+++ modules/editors/neovim.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  flake.modules.home.neovim =
+    { config, pkgs, ... }:
+    {
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        extraPackages = with pkgs; [ tree-sitter ];
+        viAlias = true;
+        vimAlias = true;
+      };
+
+      home.sessionVariables = {
+        # We use an absolute path for SUDO_EDITOR.
+        # home-manager hosts will not have the nix PATH setup and silently fall back.
+        # NOTE: this can go stale when a new HM generation is built.
+        # TODO: add a guard condition to avoid this for darwin/nixos
+        SUDO_EDITOR = "${config.programs.neovim.finalPackage}/bin/nvim";
+        VISUAL = "nvim";
+      };
+    };
+}
