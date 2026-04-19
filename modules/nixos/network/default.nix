74b99671314e125a6bf53b744dee2e5c86ff28a7
Alex Wicks <alex@awicks.io>
2026-04-19 21:27:29 +0100

    feat(network): add tailscale support

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -8,6 +8,7 @@
       nftables.enable = true;
     };
 
+    boot.initrd.systemd.network.wait-online.enable = false;
     systemd.services.NetworkManager-wait-online.enable = false;
   };
 }
--- modules/nixos/network/vpn-client.nix
+++ modules/nixos/network/vpn-client.nix
@@ -1,14 +1,26 @@
 { ... }:
 {
   aw1cks.modules.nixos.vpn-client =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     {
       environment.systemPackages = with pkgs; [
         networkmanagerapplet
         openvpn
       ];
-      networking.networkmanager.plugins = with pkgs; [
-        networkmanager-openvpn
+      networking = {
+        firewall = {
+          allowedUDPPorts = [ config.services.tailscale.port ];
+          # needed so exit nodes work
+          checkReversePath = "loose";
+          trustedInterfaces = [ "tailscale0" ];
+        };
+        networkmanager.plugins = with pkgs; [
+          networkmanager-openvpn
+        ];
+      };
+      services.tailscale.enable = true;
+      systemd.services.tailscaled.serviceConfig.Environment = [
+        "TS_DEBUG_FIREWALL_MODE=nftables"
       ];
     };
 }
