e2da44942f2a477606d13473d5727fe0907af85d
Alex Wicks <alex@awicks.io>
2026-09-05 12:41:10 +0100

    feat(java): improve maven download parallelism and use breadth-first resolver

--- modules/home/dev/java.nix
+++ modules/home/dev/java.nix
@@ -9,6 +9,10 @@
         package = pkgs.javaPackages.compiler.openjdk21;
       };
 
+      # Maven 3.9+ prepends MAVEN_ARGS to every invocation while still allowing
+      # project and command-line arguments to follow.
+      home.sessionVariables.MAVEN_ARGS = "-Dmaven.artifact.threads=16 -Daether.dependencyCollector.impl=bf -Daether.dependencyCollector.bf.threads=16";
+
       home.packages = with pkgs; [
         detekt
         maven
